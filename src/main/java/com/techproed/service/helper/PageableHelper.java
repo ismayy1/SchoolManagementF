a77df8b79465362ce7790279e1e7aab2d44ba4aa
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-02-11 20:10:08 +0200

    Car Hub

--- src/main/java/com/techproed/service/helper/PageableHelper.java
+++ src/main/java/com/techproed/service/helper/PageableHelper.java
@@ -5,18 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 @Component
 public class PageableHelper {
 
     public Pageable getPageable(int page, int size, String sort, String type) {
-
-        if (Objects.equals(type, "asc")) {
-            return PageRequest.of(page, size, Sort.by(sort).ascending());
-        } else {
-            return PageRequest.of(page, size, Sort.by(sort).descending());
-        }
-
+        return PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(type), sort));
     }
 }
