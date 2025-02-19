520caeee0558f8302d4163b4a047bbe8ddd4e5db
ismail <tkm.gala@gmail.com>
2025-02-19 10:15:29 +0200

    Backend Project

--- src/main/java/com/techproed/controller/business/LessonController.java
+++ src/main/java/com/techproed/controller/business/LessonController.java
@@ -5,11 +5,9 @@ import com.techproed.payload.response.business.LessonResponse;
 import com.techproed.payload.response.business.ResponseMessage;
 import com.techproed.service.business.LessonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,5 +24,35 @@ public class LessonController {
             @RequestBody @Valid LessonRequest lessonRequest) {
 
         return lessonService.saveLesson(lessonRequest);
+    }
+
+//    TODO edip
+    @PreAuthorize("hasAnyAuthority('Admin', 'Dean', 'ViceDean')")
+    @PostMapping("/delete/{lessonId}")
+    public ResponseMessage deleteLesson(@PathVariable Long lessonId) {
+//        return lessonService.deleteLesson(lessonId);
+        return null;
+    }
+
+    //TODO nesli
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+    @GetMapping("/getLessonByName")
+    public ResponseMessage<LessonResponse>getLessonByName(
+            @RequestParam String lessonName) {
+        //return lessonService.findLessonByName(lessonName);
+        return null;
+    }
+
+
+    //TODO ertugrul
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+    @GetMapping("/getLessonByPage")
+    public Page<LessonResponse> findLessonByPage(
+            @RequestParam(value = "page",defaultValue = "0") int page,
+            @RequestParam(value = "size",defaultValue = "10") int size,
+            @RequestParam(value = "sort",defaultValue = "lessonName") String sort,
+            @RequestParam(value = "type",defaultValue = "desc") String type) {
+        //return lessonService.getLessonByPage(page,size,sort,type);
+        return null;
     }
 }
--- src/main/java/com/techproed/service/business/LessonService.java
+++ src/main/java/com/techproed/service/business/LessonService.java
@@ -1,12 +1,16 @@
 package com.techproed.service.business;
 
+import com.techproed.entity.concretes.business.Lesson;
 import com.techproed.exception.ConflictException;
+import com.techproed.payload.mappers.LessonMapper;
 import com.techproed.payload.messages.ErrorMessages;
+import com.techproed.payload.messages.SuccessMessages;
 import com.techproed.payload.requests.business.LessonRequest;
 import com.techproed.payload.response.business.LessonResponse;
 import com.techproed.payload.response.business.ResponseMessage;
 import com.techproed.repository.business.LessonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
@@ -17,11 +21,21 @@ public class LessonService {
 
 
     private final LessonRepository lessonRepository;
+    private final LessonMapper lessonMapper;
 
     public ResponseMessage<LessonResponse> saveLesson(@Valid LessonRequest lessonRequest) {
 
 //        Validate - lesson name must be unique
         isLessonExistByName(lessonRequest.getLessonName());
+//        map DTO to Entity
+        Lesson lesson = lessonMapper.mapLessonRequestToLesson(lessonRequest);
+        Lesson savedLesson = lessonRepository.save(lesson);
+
+        return ResponseMessage.<LessonResponse>builder()
+                .returnBody(lessonMapper.mapLessonToLessonRequest(savedLesson))
+                .httpStatus(HttpStatus.CREATED)
+                .message(SuccessMessages.LESSON_SAVE)
+                .build();
     }
 
 
