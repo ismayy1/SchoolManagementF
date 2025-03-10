7a3b161f8b48b74f89c0173611d2e5fd9ba9e285
ismail <tkm.gala@gmail.com>
2025-03-10 16:52:37 +0200

    changes

--- src/main/java/com/techproed/repository/business/LessonProgramRepository.java
+++ src/main/java/com/techproed/repository/business/LessonProgramRepository.java
@@ -3,7 +3,6 @@ package com.techproed.repository.business;
 import com.techproed.entity.concretes.business.LessonProgram;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
--- src/main/java/com/techproed/repository/business/LessonRepository.java
+++ src/main/java/com/techproed/repository/business/LessonRepository.java
@@ -4,7 +4,6 @@ import com.techproed.entity.concretes.business.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
