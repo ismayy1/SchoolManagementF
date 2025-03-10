0afaaad703e638fe101affd1ba809978d2ed81e4
ismail <tkm.gala@gmail.com>
2025-03-10 16:51:02 +0200

    changes

--- src/main/java/com/techproed/payload/requests/business/AddLessonProgram.java
+++ src/main/java/com/techproed/payload/requests/business/AddLessonProgram.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
--- src/main/java/com/techproed/payload/requests/business/AddLessonProgramForStudent.java
+++ src/main/java/com/techproed/payload/requests/business/AddLessonProgramForStudent.java
@@ -3,7 +3,6 @@ package com.techproed.payload.requests.business;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
