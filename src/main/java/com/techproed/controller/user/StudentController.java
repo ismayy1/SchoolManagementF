1d96729da57ce778940509ce61c27666d6a9a6ef
ismail <tkm.gala@gmail.com>
2025-03-12 22:52:08 +0200

    changes

--- src/main/java/com/techproed/controller/user/StudentController.java
+++ src/main/java/com/techproed/controller/user/StudentController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
--- src/main/java/com/techproed/controller/user/TeacherController.java
+++ src/main/java/com/techproed/controller/user/TeacherController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
